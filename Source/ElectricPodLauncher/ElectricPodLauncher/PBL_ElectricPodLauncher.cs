fdb51b1c43d8b851b6e4d11f3b7621bea295fc81
John Turner <johnpturner8@gmail.com>
2021-07-17 17:52:38 -0400

    Initial Upload

---
+++ Source/ElectricPodLauncher/ElectricPodLauncher/PBL_ElectricPodLauncher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+using RimWorld;
+
+namespace ElectricPodLauncher
+{
+    public class PBL_ElectricPodLauncher : Building_PodLauncher
+    {
+		public override void TickRare()
+		{
+			float powerConsumption = this.TryGetComp<PBL_Chargeable>().powerConsumption;
+			//this.TryGetComp<CompPowerTrader>().Props.basePowerConsumption = powerConsumption;
+			if (this.TryGetComp<CompPowerTrader>().PowerOn)
+			{
+				this.TryGetComp<CompPowerTrader>().PowerOutput = -1 * powerConsumption;
+				this.TryGetComp<CompRefuelable>().Refuel(chargePerTick * (powerConsumption / 1000));
+			}
+		}
+		
+		//0.02 is roughly as efficient as a fueled launcher
+		public float chargePerTick = 0.1f;
+	}
+}
