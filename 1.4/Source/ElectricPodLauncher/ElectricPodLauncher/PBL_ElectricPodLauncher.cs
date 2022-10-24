b8049528b252c8466c2f36f00cb868f02c6acef1
johnpturner8 <johnpturner8@gmail.com>
2022-10-23 23:18:08 -0400

    1.4 Compatibility

---
+++ 1.4/Source/ElectricPodLauncher/ElectricPodLauncher/PBL_ElectricPodLauncher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+using RimWorld;
+
+namespace ElectricPodLauncher
+{
+    public class PBL_ElectricPodLauncher : Building_PodLauncher
+    {
+		//public override void SpawnSetup(Map map, bool respawningAfterLoad)
+  //      {
+		//	base.SpawnSetup(map, respawningAfterLoad);
+
+		//	float powerConsumption = this.TryGetComp<PBL_Chargeable>().PowerConsumption;
+		//	this.TryGetComp<CompPowerTrader>().PowerOutput = -1 * powerConsumption;
+		//}
+
+
+		public override void Tick()
+		{
+			//Debug.Log("Running 1.3");
+			float powerConsumption = this.TryGetComp<PBL_Chargeable>().PowerConsumption;
+			this.TryGetComp<CompPowerTrader>().PowerOutput = -1 * powerConsumption;
+			if (this.TryGetComp<CompPowerTrader>().PowerOn)
+			{
+				this.TryGetComp<CompRefuelable>().Refuel(chargePerTick * (powerConsumption / 1000 / 1000));
+			}
+		}
+		
+		//0.02 is roughly as efficient as a fueled launcher
+		public float chargePerTick = 0.1f;
+	}
+}
